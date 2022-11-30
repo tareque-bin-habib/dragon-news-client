65e5b98fa8327bc1a55ebeae28cf8a52d8e474c6
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-30 11:43:39 +0600

    news api added

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Category = () => {
+    const news = useLoaderData();
     return (
         <div>
-            <h2>This is Category</h2>
+            <h2>This category have {news.length} news</h2>
         </div>
     );
 };
